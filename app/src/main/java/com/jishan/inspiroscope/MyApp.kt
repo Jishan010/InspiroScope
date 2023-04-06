65d3ba2f1a549452f4decd8adb010e339535783e
“Jishan <jishan010@gmail.com>
2023-04-06 17:50:03 +0530

    If applied,this commit will setup hilt dependencies

---
+++ app/src/main/java/com/jishan/inspiroscope/MyApp.kt
@@ -0,0 +1,9 @@
+package com.jishan.inspiroscope
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+// todo update dependecies management to domain layer as well
+// add detekt setup as well
+@HiltAndroidApp
+class MyApp : Application()
--- data/src/main/java/com/jishan/data/network/QuotesService.kt
+++ data/src/main/java/com/jishan/data/network/QuotesService.kt
@@ -2,22 +2,8 @@ package com.jishan.data.network
 
 import com.jishan.data.models.QuoteResponse
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
-class QuotesService {
-    private val baseUrl = "https://quotes.rest/"
-
-    private val quotesApi: QuotesApi
-
-    init {
-        val retrofit = Retrofit.Builder()
-            .baseUrl(baseUrl)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-        quotesApi = retrofit.create(QuotesApi::class.java)
-    }
+class QuotesService(private val quotesApi: QuotesApi) {
 
     suspend fun getRandomQuote(): Response<QuoteResponse> {
         return quotesApi.getRandomQuote()
