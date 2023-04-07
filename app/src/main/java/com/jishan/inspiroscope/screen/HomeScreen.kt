86f8041a12d682f6515f243e0a0cfbc2406dd66c
“Jishan <jishan010@gmail.com>
2023-04-07 17:12:29 +0530

    if applied,this will fix ktlint issues

--- app/src/main/java/com/jishan/inspiroscope/screen/HomeScreen.kt
+++ app/src/main/java/com/jishan/inspiroscope/screen/HomeScreen.kt
@@ -1,12 +1,15 @@
 package com.jishan.inspiroscope.screen
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.VerticalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -20,31 +23,39 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.rememberAsyncImagePainter
 import com.jishan.domain.entitiy.QuoteEntity
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun HomeScreen() {
     val homeViewModel = hiltViewModel<HomeViewModel>()
     val quote = homeViewModel.quote.collectAsState()
     val wallpaper = homeViewModel.wallpaper.collectAsState()
 
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        wallpaper.value?.url?.let { url ->
-            Image(
-                painter = rememberAsyncImagePainter(url),
-                contentDescription = "Wallpaper",
-                modifier = Modifier.fillMaxSize(),
-                contentScale = ContentScale.Crop
-            )
+    val listState = rememberPagerState()
+
+    VerticalPager(
+        pageCount = Int.MAX_VALUE, // Infinite pages
+        state = listState,
+        modifier = Modifier.fillMaxSize()
+    ) { page ->
+        homeViewModel.loadNextData(page)
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            wallpaper.value?.url?.let { url ->
+                Image(
+                    painter = rememberAsyncImagePainter(url),
+                    contentDescription = "Wallpaper",
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.Crop
+                )
+            }
+            quote.value?.let { QuoteCard(it) }
         }
-        quote.value?.let { QuoteCard(it) }
     }
 }
 
 @Composable
 fun QuoteCard(quoteEntity: QuoteEntity) {
     Column(
-        modifier = Modifier
-            .background(color = Color.Black.copy(alpha = 0.3f))
-            .padding(16.dp),
+        modifier = Modifier.background(color = Color.Black.copy(alpha = 0.3f)).padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
