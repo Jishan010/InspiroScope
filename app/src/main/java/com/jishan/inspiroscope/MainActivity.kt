a96ef01c798779ed761c727a82fb291a42dd76d7
“Jishan <jishan010@gmail.com>
2023-04-07 18:16:34 +0530

    if applied,this commit will remove status bar

--- app/src/main/java/com/jishan/inspiroscope/MainActivity.kt
+++ app/src/main/java/com/jishan/inspiroscope/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.jishan.inspiroscope
 
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -19,12 +23,24 @@ class MainActivity : ComponentActivity() {
             InspiroScopeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
+
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
                     HomeScreen()
                 }
             }
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+            window.insetsController?.let { controller ->
+                controller.systemBarsBehavior =
+                    WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+                controller.hide(WindowInsets.Type.statusBars())
+            }
+        } else {
+            @Suppress("DEPRECATION") window.decorView.systemUiVisibility =
+                (View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
         }
     }
 }
